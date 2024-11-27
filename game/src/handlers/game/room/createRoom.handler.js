6e70033632b0e38402995518cc3f2c693d0fbe70
Young-Hyun Kwon <ehe151@naver.com>
2024-11-27 19:30:16 +0900

    update : createRoom, joinRoom userSessions add user
    - 방을 만들거나 참가한 유저를
      유저세션 배열에 추가해주는 로직 추가

--- game/src/handlers/game/room/createRoom.handler.js
+++ game/src/handlers/game/room/createRoom.handler.js
@@ -2,7 +2,7 @@ import CustomError from '../../../Error/custom.error.js';
 import { ErrorCodesMaps } from '../../../Error/error.codes.js';
 import { sendCreateRoomResponse } from '../../../response/room/room.response.js';
 import { addGameSession } from '../../../sessions/game.session.js';
-import { getUserById } from '../../../sessions/user.sessions.js';
+import { addUser, getUserById } from '../../../sessions/user.sessions.js';
 import { v4 as uuidv4 } from 'uuid';
 
 export const createRoomHandler = async ({ socket, payload }) => {
@@ -10,11 +10,8 @@ export const createRoomHandler = async ({ socket, payload }) => {
 
   // ------------ TODO: token 검증 ---------------
 
-  // 유저 세션 검증
-  const user = getUserById(socket.userId);
-  if (!user) {
-    throw new CustomError(ErrorCodesMaps.USER_NOT_FOUND);
-  }
+  // 방을 만든 유저를 userSessions에 추가시켜준다.
+  const user = addUser(userId, socket);
 
   // 게임 생성
   const gameUUID = uuidv4();
