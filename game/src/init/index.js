17697d09cf131ad9c3ff276d5ab047ce8e11a59d
Young-Hyun Kwon <ehe151@naver.com>
2024-12-03 11:41:17 +0900

    add : assets json 파일 추가 및 load 기능 구현
    - 게임 데이터 테이블을 json으로 변환하여 관리한다.
    - 서버가 실행하면 loadGameAssets() 호출하여 메모리에 저장한다.
    - getGameAssets()로 asset 데이터를 가져와 사용하면 된다.

--- game/src/init/index.js
+++ game/src/init/index.js
@@ -1,8 +1,10 @@
+import { loadServerGameAssets } from './load.assets.js';
 import { loadProtos } from './load.protos.js';
 
 const initServer = async () => {
   try {
-    loadProtos();
+    await loadServerGameAssets();
+    await loadProtos();
   } catch (e) {
     console.error(e);
     process.exit(1);
