2ab804017c0a50428d2dd8433375abd3d6004401
soonode97 <soonode951@gmail.com>
2024-12-05 16:49:54 +0900

    fix: 귀신 소환 요청 핸들러에서 소환 지점과 아이디를 서버에서 생성하고 응답하도록 수정
    1. GhostSpawnRequest에 ghostTypeId만 받는 것으로 수정되면서 서버에서 소환 위치와 아이디를 부여하고 Notification 날려주는 방향으로 변경
    
    2. 호스트에게도 귀신 소환 Noti 날려주도록 변경

--- game/src/handlers/game/ghost/ghostSpawn.handler.js
+++ game/src/handlers/game/ghost/ghostSpawn.handler.js
@@ -4,15 +4,14 @@ import { getGameSessionById } from '../../../sessions/game.session.js';
 import { getUserById } from '../../../sessions/user.sessions.js';
 import Ghost from '../../../classes/models/ghost.class.js';
 import { ghostSpawnNotification } from '../../../notifications/ghost/ghost.notification.js';
+import { getRandomInt } from '../../../utils/math/getRandomInt.js';
 
 /**
  * 귀신의 특수 상태 요청에 대한 핸들러 함수입니다. (호스트만 요청)
  */
 export const ghostSpawnHandler = ({ socket, payload }) => {
   try {
-    const { ghostInfo } = payload;
-
-    const { position, rotation } = ghostInfo.moveInfo;
+    const { ghostTypeId } = payload;
 
     const user = getUserById(socket.userId);
     if (!user) {
@@ -24,14 +23,44 @@ export const ghostSpawnHandler = ({ socket, payload }) => {
       throw new CustomError(ErrorCodesMaps.GAME_NOT_FOUND);
     }
 
+    const positionIndex = getRandomInt(
+      0,
+      gameSession.ghostSpawnPositions.length,
+    );
+    const ghostPosition = gameSession.ghostSpawnPositions.splice(
+      positionIndex,
+      1,
+    );
+    const positions = ghostPosition[0].GhostSpawnPos.split(',').map(
+      (position) => {
+        return Number(position);
+      },
+    );
+    const rotation = { x: 0, y: 0, z: 0 };
+    const position = {
+      x: positions[0],
+      y: positions[1],
+      z: positions[2],
+    };
+
     const ghost = new Ghost(
-      ghostInfo.id,
-      ghostInfo.ghostTypeId,
+      gameSession.ghostIdCount++,
+      ghostTypeId,
       position,
       rotation,
     );
-
     gameSession.addGhost(ghost);
+
+    const moveInfo = {
+      position,
+      rotation,
+    };
+
+    const ghostInfo = {
+      ghostId: ghost.id,
+      ghostTypeId,
+      moveInfo,
+    };
 
     ghostSpawnNotification(gameSession, ghostInfo);
   } catch (e) {
